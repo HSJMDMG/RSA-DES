781391dcf4c5a41ca371105c92a989af288b342b
HSJMDMG <lynj.y@qq.com>
2016-11-19 23:53:51 +0800

    finish the major part, can deal with 32-bits numbers, reconstructing codes to fit the 128bits

--- src/main.js
+++ src/main.js
@@ -1,158 +1,159 @@
-
-
 fileContent = '';
 fileName = '';
-outputContent ='';
+outputContent = '';
 keyContent = '';
 
 function checkKey1() {
-  document.getElementById('alertbar1').innerHTML="";
-  key = d3.select('#key1').property('value');
+    document.getElementById('alertbar1').innerHTML = "";
+    key = d3.select('#key1').property('value');
 
-  //check for length
-  keylen = 16;
-  if (key.length > keylen) {
-    alertbar = d3.select('#alertbar1');
-    alertblock = alertbar.append('div');
-    alertblock.attr('class', 'alert alert-danger');
-    alertblock.append('button')
-              .attr('type', 'button')
-              .attr('class', 'close')
-              .attr('data-dismiss', 'alert')
-              .text('×');
-    alertblock.append('h4')
-              .text('提示！');
-    alertblock.append('p')
-              .text('密钥过长，超过16位，请重新输入！');
+    //check for length
+    keylen = 2;
+    if (key.length > keylen) {
+        alertbar = d3.select('#alertbar1');
+        alertblock = alertbar.append('div');
+        alertblock.attr('class', 'alert alert-danger');
+        alertblock.append('button')
+            .attr('type', 'button')
+            .attr('class', 'close')
+            .attr('data-dismiss', 'alert')
+            .text('×');
+        alertblock.append('h4')
+            .text('提示！');
+        alertblock.append('p')
+            .text('密钥过长，超过16位，请重新输入！');
 
-    return false;
-  }
-  if (key.length < keylen) {
-    alertbar = d3.select('#alertbar1');
-    alertblock = alertbar.append('div');
-    alertblock.attr('class', 'alert alert-danger');
-    alertblock.append('button')
-              .attr('type', 'button')
-              .attr('class', 'close')
-              .attr('data-dismiss', 'alert')
-              .text('×');
-    alertblock.append('h4')
-              .text('提示！');
-    alertblock.append('p')
-              .text('密钥过短，少于16位，请重新输入！');
+        return false;
+    }
+    if (key.length < keylen) {
+        alertbar = d3.select('#alertbar1');
+        alertblock = alertbar.append('div');
+        alertblock.attr('class', 'alert alert-danger');
+        alertblock.append('button')
+            .attr('type', 'button')
+            .attr('class', 'close')
+            .attr('data-dismiss', 'alert')
+            .text('×');
+        alertblock.append('h4')
+            .text('提示！');
+        alertblock.append('p')
+            .text('密钥过短，少于16位，请重新输入！');
 
-    return false;
-  }
+        return false;
+    }
 
-  function invalid(ch) {
-    if ('0' <= ch && ch <= '9') return false;
-    if ('a' <= ch && ch <='z') return false;
-    if ('A' <= ch && ch <='Z') return false;
-    return true;
-  }
+    function invalid(ch) {
+        if ('0' <= ch && ch <= '9') return false;
+        if ('a' <= ch && ch <= 'z') return false;
+        if ('A' <= ch && ch <= 'Z') return false;
+        return true;
+    }
 
-  //check for valid character
-  for (var i = 0; i < keylen; i++) {
-    if (invalid(key[i])) {
-      alertbar = d3.select('#alertbar1');
-      alertblock = alertbar.append('div');
-      alertblock.attr('class', 'alert alert-danger');
-      alertblock.append('button')
+    //check for valid character
+    for (var i = 0; i < keylen; i++) {
+        if (invalid(key[i])) {
+            alertbar = d3.select('#alertbar1');
+            alertblock = alertbar.append('div');
+            alertblock.attr('class', 'alert alert-danger');
+            alertblock.append('button')
                 .attr('type', 'button')
                 .attr('class', 'close')
                 .attr('data-dismiss', 'alert')
                 .text('×');
-      alertblock.append('h4')
+            alertblock.append('h4')
                 .text('提示！');
-      alertblock.append('p')
+            alertblock.append('p')
                 .text('密钥中出现非法字符，请修改！');
-      return false;
+            return false;
+        }
     }
-  }
 
-  //d3.select('#key1').attr('disabled','');
-  rsa_key = RSA_encode(key);
+    //d3.select('#key1').attr('disabled','');
+    rsa_key = RSA_encode(key);
 
-  d3.select('#rsakey1').property('value', rsa_key);
-  d3.select('#rsa1').classed('hidden', false);
-  keyContent = key;
-  return true;
+    d3.select('#rsakey1').property('value', rsa_key);
+    d3.select('#rsa1').classed('hidden', false);
+    keyContent = key;
+    return true;
 }
+
 function checkKey2() {
-  document.getElementById('alertbar2').innerHTML="";
-  key = d3.select('#key2').property('value');
+    document.getElementById('alertbar2').innerHTML = "";
+    key = d3.select('#key2').property('value');
 
-  //check for length
-  keylen = 16;
-  if (key.length > keylen) {
-    alertbar = d3.select('#alertbar2');
-    alertblock = alertbar.append('div');
-    alertblock.attr('class', 'alert alert-danger');
-    alertblock.append('button')
-              .attr('type', 'button')
-              .attr('class', 'close')
-              .attr('data-dismiss', 'alert')
-              .text('×');
-    alertblock.append('h4')
-              .text('提示！');
-    alertblock.append('p')
-              .text('密钥过长，超过16位，请重新输入！');
+    function invalid(ch) {
+        if ('0' <= ch && ch <= '9') return false;
+        if ('a' <= ch && ch <= 'z') return false;
+        if ('A' <= ch && ch <= 'Z') return false;
+        return true;
+    }
 
-    return false;
-  }
-  if (key.length < keylen) {
-    alertbar = d3.select('#alertbar2');
-    alertblock = alertbar.append('div');
-    alertblock.attr('class', 'alert alert-danger');
-    alertblock.append('button')
-              .attr('type', 'button')
-              .attr('class', 'close')
-              .attr('data-dismiss', 'alert')
-              .text('×');
-    alertblock.append('h4')
-              .text('提示！');
-    alertblock.append('p')
-              .text('密钥过短，少于16位，请重新输入！');
-
-    return false;
-  }
-
-  function invalid(ch) {
-    if ('0' <= ch && ch <= '9') return false;
-    if ('a' <= ch && ch <='z') return false;
-    if ('A' <= ch && ch <='Z') return false;
-    return true;
-  }
-
-  //check for valid character
-  for (var i = 0; i < keylen; i++) {
-    if (invalid(key[i])) {
-      alertbar = d3.select('#alertbar2');
-      alertblock = alertbar.append('div');
-      alertblock.attr('class', 'alert alert-danger');
-      alertblock.append('button')
+    //check for valid character
+    for (var i = 0; i < key.length; i++) {
+        if (invalid(key[i])) {
+          console.log(i);
+            alertbar = d3.select('#alertbar2');
+            alertblock = alertbar.append('div');
+            alertblock.attr('class', 'alert alert-danger');
+            alertblock.append('button')
                 .attr('type', 'button')
                 .attr('class', 'close')
                 .attr('data-dismiss', 'alert')
                 .text('×');
-      alertblock.append('h4')
+            alertblock.append('h4')
                 .text('提示！');
-      alertblock.append('p')
+            alertblock.append('p')
                 .text('密钥中出现非法字符，请修改！');
-      return false;
+            return false;
+        }
     }
-  }
 
-//  d3.select('#key2').attr('disabled','');
+    //  d3.select('#key2').attr('disabled','');
 
-  rsa_key = RSA_decode(key);
+    rsa_key = RSA_decode(key);
+    key = rsa_key;
+    //check for length
+  /*  keylen = 16;
+    if (key.length > keylen) {
+        alertbar = d3.select('#alertbar2');
+        alertblock = alertbar.append('div');
+        alertblock.attr('class', 'alert alert-danger');
+        alertblock.append('button')
+            .attr('type', 'button')
+            .attr('class', 'close')
+            .attr('data-dismiss', 'alert')
+            .text('×');
+        alertblock.append('h4')
+            .text('提示！');
+        alertblock.append('p')
+            .text('密钥过长，超过32位，请重新输入！');
 
-  d3.select('#rsakey2').property('value', rsa_key);
-  d3.select('#rsa2').classed('hidden', false);
+        return false;
+    }
+    if (key.length < keylen) {
+        alertbar = d3.select('#alertbar2');
+        alertblock = alertbar.append('div');
+        alertblock.attr('class', 'alert alert-danger');
+        alertblock.append('button')
+            .attr('type', 'button')
+            .attr('class', 'close')
+            .attr('data-dismiss', 'alert')
+            .text('×');
+        alertblock.append('h4')
+            .text('提示！');
+        alertblock.append('p')
+            .text('密钥过短，少于32位，请重新输入！');
+
+        return false;
+    }
+
+*/
+    d3.select('#rsakey2').property('value', rsa_key);
+    d3.select('#rsa2').classed('hidden', false);
 
 
-  keyContent = rsa_key;
-  return true;
+    keyContent = rsa_key;
+    return true;
 }
 
 
@@ -162,76 +163,76 @@ function checkFile(i) {
     filename = d3.select('#inputFile' + i).property('value');
 
     var pos = filename.lastIndexOf(".");
-    var lastname = filename.substring(pos,filename.length);  //此处文件后缀名也可用数组方式获得str.split(".")
+    var lastname = filename.substring(pos, filename.length); //此处文件后缀名也可用数组方式获得str.split(".")
 
     function fileInvalid(str) {
-      for (var i = 0; i < str.length; i++)
-        if ((str[i] != '0') && (str[i] != '1')) return true;
-      return false;
+        for (var i = 0; i < str.length; i++)
+            if ((str[i] != '0') && (str[i] != '1')) return true;
+        return false;
     }
-    if ((lastname.toLowerCase()!=".txt") || fileInvalid(fileContent)) {
-      alertbar = d3.select('#alertbar' + i);
-      alertblock = alertbar.append('div');
-      alertblock.attr('class', 'alert alert-danger');
-      alertblock.append('button')
-                .attr('type', 'button')
-                .attr('class', 'close')
-                .attr('data-dismiss', 'alert')
-                .text('×');
-      alertblock.append('h4')
-                .text('提示！');
-      alertblock.append('p')
-                .text('不是有效的txt文件，请重新上传！');
+    if ((lastname.toLowerCase() != ".txt") || fileInvalid(fileContent)) {
+        alertbar = d3.select('#alertbar' + i);
+        alertblock = alertbar.append('div');
+        alertblock.attr('class', 'alert alert-danger');
+        alertblock.append('button')
+            .attr('type', 'button')
+            .attr('class', 'close')
+            .attr('data-dismiss', 'alert')
+            .text('×');
+        alertblock.append('h4')
+            .text('提示！');
+        alertblock.append('p')
+            .text('不是有效的txt文件，请重新上传！');
 
-      return false;
-    }
-    else
-      {
+        return false;
+    } else {
         pos = filename.lastIndexOf("\\");
         fileName = filename.substring(pos + 1, filename.length - 4);
         return true;
-      }
+    }
 }
 
 function upload(input) {
-  var xmlDoc;
-  //支持chrome IE10
+    var xmlDoc;
+    //支持chrome IE10
 
-  if (window.FileReader) {
-    var file = input.files[0];
-    filename = file.name.split(".")[0];
-    var reader = new FileReader();
-    reader.onload = function() {
-      /*console.log(this.result);
-      alert(this.result);*/
-      fileContent = this.result;
-    };
-    reader.readAsText(file);
-  }
-  //支持IE 7 8 9 10
-  else if (typeof window.ActiveXObject != 'undefined'){
+    if (window.FileReader) {
+        var file = input.files[0];
+        filename = file.name.split(".")[0];
+        var reader = new FileReader();
+        reader.onload = function() {
+            /*console.log(this.result);
+            alert(this.result);*/
+            fileContent = this.result;
+        };
+        reader.readAsText(file);
+    }
+    //支持IE 7 8 9 10
+    else if (typeof window.ActiveXObject != 'undefined') {
 
-    xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-    xmlDoc.async = false;
-    xmlDoc.load(input.value);
-    //alert(xmlDoc.xml);
-    fileContent = xmlDoc.xml;
-  }
-  //支持FF
-  else if (document.implementation && document.implementation.createDocument) {
-    xmlDoc = document.implementation.createDocument("", "", null);
-    xmlDoc.async = false;
-    xmlDoc.load(input.value);
-    fileContent = xmlDoc.xml;
-  } else {
-    alert('不支持这种浏览器，请换一种试试');
-  }
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = false;
+        xmlDoc.load(input.value);
+        //alert(xmlDoc.xml);
+        fileContent = xmlDoc.xml;
+    }
+    //支持FF
+    else if (document.implementation && document.implementation.createDocument) {
+        xmlDoc = document.implementation.createDocument("", "", null);
+        xmlDoc.async = false;
+        xmlDoc.load(input.value);
+        fileContent = xmlDoc.xml;
+    } else {
+        alert('不支持这种浏览器，请换一种试试');
+    }
 }
 
 function doSave(value, type, name) {
     var blob;
     if (typeof window.Blob == "function") {
-        blob = new Blob([value], {type: type});
+        blob = new Blob([value], {
+            type: type
+        });
     } else {
         var BlobBuilder = window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder;
         var bb = new BlobBuilder();
@@ -258,74 +259,74 @@ function doSave(value, type, name) {
 }
 
 function saveFile() {
-  doSave(outputContent, "text", fileName + '_output.txt');
+    doSave(outputContent, "text", fileName + '_output.txt');
 }
 
 
 
 function Work(i) {
-  console.log(i);
-  if (i == 1)
-    valid = checkKey1() & checkFile(1);
-  else {
-    valid = checkKey2() & checkFile(2);
-  }
+    console.log(i);
+    if (i == 1)
+        valid = checkKey1() & checkFile(1);
+    else {
+        valid = checkKey2() & checkFile(2);
+    }
 
-  if (!valid) return ;
-  //console.log(fileContent);
-  //console.log(fileName);
+    if (!valid) return;
+    //console.log(fileContent);
+    //console.log(fileName);
 
-  //outputContent = fileContent;
-  console.log(keyContent);
+    //outputContent = fileContent;
+    console.log(keyContent);
 
-  if (i == 1) encode = true; else encode = false;
-  alertbar = d3.select('#alertbar' + i);
-  alertblock = alertbar.append('div');
+    if (i == 1) encode = true;
+    else encode = false;
+    alertbar = d3.select('#alertbar' + i);
+    alertblock = alertbar.append('div');
 
-  if ((!encode) && (fileContent.length % 64 !== 0)) {
+    if ((!encode) && (fileContent.length % 64 !== 0)) {
 
-    alertblock.attr('class', 'alert alert-danger');
-    alertblock.append('button')
-              .attr('type', 'button')
-              .attr('class', 'close')
-              .attr('data-dismiss', 'alert')
-              .text('×');
-    alertblock.append('h4')
-              .text('提示！');
-    alertblock.append('p')
-              .text('解密失败！请检查密文长度！');
-
-
-    return;
-
-  }
-  console.log(fileContent);
-
-  outputContent = CBC_DES(fileContent, keyContent, encode);
-
-
-  alertblock.attr('class', 'alert alert-success');
-  alertblock.append('button')
+        alertblock.attr('class', 'alert alert-danger');
+        alertblock.append('button')
             .attr('type', 'button')
             .attr('class', 'close')
             .attr('data-dismiss', 'alert')
             .text('×');
-  alertblock.append('h4')
+        alertblock.append('h4')
             .text('提示！');
-  if (encode) {
-    alertblock.append('p')
-            .text('加密成功！');
-  }
-  else {
-    alertblock.append('p')
-            .text('解密成功！');
-  }
+        alertblock.append('p')
+            .text('解密失败！请检查密文长度！');
 
-  //d3.select('#key').attr('disabled',false);
-  $("#key").removeAttr("disabled");
-  d3.select('#output' + i).classed('hidden', false);
-  d3.select('#result' + i).text(outputContent);
-  //  saveFile();
+
+        return;
+
+    }
+    console.log(fileContent);
+
+    outputContent = CBC_DES(fileContent, keyContent, encode);
+
+
+    alertblock.attr('class', 'alert alert-success');
+    alertblock.append('button')
+        .attr('type', 'button')
+        .attr('class', 'close')
+        .attr('data-dismiss', 'alert')
+        .text('×');
+    alertblock.append('h4')
+        .text('提示！');
+    if (encode) {
+        alertblock.append('p')
+            .text('加密成功！');
+    } else {
+        alertblock.append('p')
+            .text('解密成功！');
+    }
+
+    //d3.select('#key').attr('disabled',false);
+    $("#key").removeAttr("disabled");
+    d3.select('#output' + i).classed('hidden', false);
+    d3.select('#result' + i).text(outputContent);
+    //  saveFile();
 
 
 }
--- src/rsa.js
+++ src/rsa.js
@@ -1,8 +1,282 @@
-function RSA_encode(key) {
-  return key;
+var p, q, e, d, n, t;
+
+
+function exp(x, k, mod) {
+    if (k === 0) return 1;
+    if (k === 1) return x;
+
+    var ans = 0;
+    ans = exp(x, k / 2, mod);
+    ans = (ans * ans) % mod;
+    if (k & 1) ans = (ans * x) % mod;
+
+    return ans;
+}
+
+function ExtendEuclid(a, b, parameter) {
+    if (b === 0) {
+        parameter.x = 1;
+        parameter.y = 0;
+        return a;
+    }
+    var tmp;
+    tmp = parameter.x;
+    parameter.x = parameter.y;
+    parameter.y = tmp;
+
+
+    var gcd = ExtendEuclid(b, a % b, parameter);
+
+    tmp = parameter.x;
+    parameter.x = parameter.y;
+    parameter.y = tmp;
+
+
+    parameter.y -= parseInt((a / b)) * parameter.x;
+    return gcd;
 }
 
 
-function RSA_decode(key) {
-  return key;
+function GenerateKeyPair_smallnum_bak() {
+
+
+    function show_ne() {
+        d3.select('#rsa-output').classed('hidden', false);
+        d3.select('#keypair').text("n:" + n.toString(16) + "          e:" + e.toString(16));
+
+        d3.select('#secret').text("p:" + p.toString(16) + "          q:" + q.toString(16) + "          d:" + d.toString(16));
+
+
+    }
+
+
+    function GeneratePrime() {
+        // p : 64bit, q: 65 bit
+        function checkPrime(n) {
+            // Use Miller-Rabin Method
+            if (n === 1) return false;
+
+            sp = [3, 5, 7, 9, 11];
+            for (i = 0; i < sp.length; i++)
+                if (n % sp[i] === 0) return false;
+
+            m = n - 1;
+            k = 0;
+            while ((m & 1) === 0) {
+                k++;
+                m >>= 1;
+            }
+
+            times = 5;
+            for (i = 0; i < times; i++) {
+                // a in [2, n - 2]
+                a = (Math.floor(n * Math.random()) + 1) % (n - 1) + 1;
+                b = exp(a, m, n);
+
+                if (b === 1 || b == (n - 1)) continue;
+                pass = false;
+                for (var j = 0; j < k; j++) {
+                    b = (b * b) % n;
+                    // if (b === 1) return false;
+                    if (b == (n - 1)) {
+                        pass = true;
+                        break;
+                    }
+                }
+                if (!pass) return false;
+            }
+
+            return true;
+
+
+        }
+        var tp, tq;
+        tp = 1;
+        tq = 1;
+        lenp = 5;
+        lenq = 5;
+
+        while (!checkPrime(tp)) {
+            tp = 1;
+            for (i = 0; i < lenp - 2; i++) {
+              //  console.log(tp);
+                bit = (Math.random() - 0.5 > 0) ? 1 : 0;
+                tp = (tp << 1) + bit;
+            }
+          //  console.log(tp);
+            tp = (tp << 1) + 1;
+          //  console.log(tp);
+        }
+
+
+        while (!checkPrime(tq)) {
+            tq = 1;
+            for (i = 0; i < lenq - 2; i++) {
+                bit = (Math.random() - 0.5 > 0) ? 1 : 0;
+                tq = (tq << 1) + bit;
+            }
+            tq = (tq << 1) + 1;
+        }
+
+        p = tp;
+        q = tq;
+        console.log(p, q);
+
+
+
+    }
+
+
+    GeneratePrime();
+    n = p * q;
+    phin = (p - 1) * (q - 1);
+    d = parseInt(Math.random() * n);
+
+    parameter = {
+        x: 0,
+        y: 0
+    };
+    while (d === 0 || ExtendEuclid(d, phin, parameter) != 1) {
+        d = parseInt(Math.random() * n);
+    }
+
+    e = (parameter.x % phin + phin) % phin;
+    console.log(d, e, phin);
+
+    show_ne();
+}
+
+
+
+function GenerateKeyPair() {
+
+
+    function show_ne() {
+        d3.select('#rsa-output').classed('hidden', false);
+        d3.select('#keypair').text("n:" + n.toString(16) + "          e:" + e.toString(16));
+
+        d3.select('#secret').text("p:" + p.toString(16) + "          q:" + q.toString(16) + "          d:" + d.toString(16));
+
+
+    }
+
+
+    function GeneratePrime() {
+        // p : 64bit, q: 65 bit
+        function checkPrime(n) {
+            // Use Miller-Rabin Method
+            if (n.equals(1)) return false;
+
+            sp = [3, 5, 7, 9, 11];
+            for (i = 0; i < sp.length; i++)
+                if (n.mod(sp[i]) === 0) return false;
+
+            m = n.substract(1);
+            k = 0;
+            while ((m.mod(2)) === 0) {
+                k++;
+                m = m.shiftRight(1);
+            }
+
+            times = 5;
+            for (i = 0; i < times; i++) {
+                // a in [2, n - 2]
+                a = (Math.floor(n * Math.random()) + 1) % (n - 1) + 1;
+                b = exp(a, m, n);
+
+                if (b === 1 || b == (n - 1)) continue;
+                pass = false;
+                for (var j = 0; j < k; j++) {
+                    b = (b * b) % n;
+                    // if (b === 1) return false;
+                    if (b == (n - 1)) {
+                        pass = true;
+                        break;
+                    }
+                }
+                if (!pass) return false;
+            }
+
+            return true;
+
+
+        }
+        var tp, tq;
+        tp = new BigInteger('1');
+        tq = new BigInteger('1');
+
+        lenp = 64;
+        lenq = 65;
+
+        while (!checkPrime(tp)) {
+
+            for (i = 0; i < lenp - 2; i++) {
+                console.log(tp.toString());
+                bit = (Math.random() - 0.5 > 0) ? 1 : 0;
+                tp = tp.shiftLeft(1);
+                console.log(tp.toString());
+                tp = tp.add(new BigInteger(bit.toString()));
+            }
+          //  console.log(tp);
+          tp = tp.shiftLeft(1);
+          tp = tp.add(bit.toString());
+          //  console.log(tp);
+        }
+
+    //    console.log(tp.toString());
+
+/*
+        while (!checkPrime(tq)) {
+            tq = 1;
+            for (i = 0; i < lenq - 2; i++) {
+                bit = (Math.random() - 0.5 > 0) ? 1 : 0;
+                tq = (tq << 1) + bit;
+            }
+            tq = (tq << 1) + 1;
+        }
+
+        p = tp;
+        q = tq;
+        console.log(p, q);
+
+*/
+
+    }
+
+
+    GeneratePrime();
+    n = p * q;
+    phin = (p - 1) * (q - 1);
+    d = parseInt(Math.random() * n);
+
+    parameter = {
+        x: 0,
+        y: 0
+    };
+    while (d === 0 || ExtendEuclid(d, phin, parameter) != 1) {
+        d = parseInt(Math.random() * n);
+    }
+
+    e = (parameter.x % phin + phin) % phin;
+    console.log(d, e, phin);
+
+    show_ne();
+}
+
+
+
+function RSA_encode(m) {
+    m = parseInt(m, 16);
+    c = exp(m, e, n);
+    c = c.toString(16);
+    return c;
+}
+
+
+function RSA_decode(c) {
+    c = parseInt(c, 16);
+    m = exp(c, d, n);
+    m = m.toString(16);
+
+    return m;
 }
